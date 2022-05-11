803fa996c15faee32efa4c46bb614ace558743cd
zakaria-root <elmourtazakezia@gmail.com>
2022-05-11 23:59:43 +0100

    feat: ajouter travail 5,6,7

---
+++ CPP_ELMOURTAZAK_ZAKARIA/Exam/2019-2020/elmourtazak_travail_7.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <math.h>
+
+class segment
+{
+private:
+    double x, y, z, t;
+    inline static int count{0};
+
+public:
+    segment()
+    {
+        std::cout << "la creation de nouveau segment " << std::endl;
+        std::cout << "il rest " << ++count << " segment" << std::endl;
+        this->x = 0;
+        this->y = 0;
+        this->z = 10;
+        this->t = 5;
+    };
+    void definir(double x, double y, double z, double t)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->t = t;
+    }
+    ~segment()
+    {
+        std::cout << "fin de lobejt segment evec les cordonnees :   ";
+        afficher();
+        std::cout << "il rest " << --count << " segment ." << std::endl;
+    }
+    double langueur()
+    {
+        return sqrt(pow(x - z, 2) + pow(y - t, 2));
+    }
+    void afficher()
+    {
+        std::cout << "x = " << x << ", y = " << y << ", z = " << z << ", t = " << t << std::endl;
+    }
+    void afficher(const char *nom)
+    {
+        std::cout << "l'utilisateur " << nom << " a les cordonnees : " << std::endl;
+        afficher();
+    }
+    bool coincide(segment s)
+    {
+        return (x == s.x && y == s.y) || (z == s.z && s.t == t);
+    }
+};
+
+main()
+{
+    segment s0;
+    s0.afficher("zindi");
+    std::cout << "---------" << std::endl;
+    getchar();
+    s0.afficher();
+}
